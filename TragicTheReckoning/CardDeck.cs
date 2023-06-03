42bd6b2753e05e3bbf38ddc71088679e646c65e6
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-06-03 23:57:55 +0100

    Add Card deck shuffle

--- TragicTheReckoning/CardDeck.cs
+++ TragicTheReckoning/CardDeck.cs
@@ -8,7 +8,11 @@ namespace TragicTheReckoning
     public class CardDeck
     {
         private List<Card> listofcards=new List<Card>();
+        private List<Card> newlist=new List<Card>();
         private int a;
+        private static Random random= new Random();
+        private int b;
+        
         
         public CardDeck()
         {
@@ -22,7 +26,8 @@ namespace TragicTheReckoning
             AddToList(listofcards,CardNames.TimTheWizard, 1);
             AddToList(listofcards,CardNames.SharplyDressed, 1);
             AddToList(listofcards,CardNames.BlueSteel, 2);
-           
+
+            Shuffle<Card>(listofcards); 
         }
 
         public List<Card> AddToList(List<Card> listofcards,CardNames cardname,int i)
@@ -39,6 +44,21 @@ namespace TragicTheReckoning
         public List<Card> RemoveList(List<Card> listofcards,int i)
         {
             listofcards.RemoveAt( i );
+            return listofcards;
+        }
+        public List<TragicTheReckoning.Card> Shuffle<Card>( List<TragicTheReckoning.Card> listofcards)
+        {
+            a=0;
+            while(a<20)
+            {
+                a++;
+                b=random.Next(0,20-a);
+                newlist[a]=listofcards[b];
+                RemoveList(listofcards,b);
+
+            }
+            listofcards=newlist;
+
             return listofcards;
         }
     }
